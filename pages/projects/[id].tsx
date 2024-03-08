e01d2a36a9f0e826d276502c03bd3ee9d7625ba0
kelly-keating <keating.kel@gmail.com>
2024-03-08 16:56:13 +1300

    add initial project page structure

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,22 +1,52 @@
+import { Tech } from "../../models/projects"
 import { useRouter } from "next/router"
-import { projects } from "../../api/hardData"
+import { projects, tech } from "../../api/hardData"
 
 import Page404 from "../404"
 import InProgress from "../../components/InProgress"
+import { useMemo } from "react"
 
 export default function Project() {
   const router = useRouter()
   const id = router.query.id
 
   const proj = projects.find(p => p.id === id)
+  // TODO: sort tag style (plus bright/dark)
+  const tags = proj?.tech.map((tech) => (
+      <div key={tech.id} style={{
+        color: tech.color || "aqua",
+      }}>
+        <p>{tech.text}</p>
+      </div>
+    )) || null
 
   if(!proj) return <Page404 />
-
-  return (
+  if(!proj.details) return (
     <div className="projects_container">
       <h2>{proj.name}</h2>
-
       <InProgress />
+    </div>
+  )
+
+  // TODO: add back button
+  return (
+    <div className="projects_container">
+      <div className="project_header">
+        <h2>{proj.name}</h2>
+        <div>
+          {proj.url_deploy && <p>site link</p>}
+          {proj.url_npm && <p>npm link</p>}
+          {proj.url_github && <p>github link</p>}
+        </div>
+      </div>
+      <div>
+        <p>{proj.blurb}</p>
+        {tags}
+      </div>
+      <div>
+        <img className="project_display" src={proj.image} />
+      </div>
+      <div>{proj.details}</div>
     </div>
   )
 }
