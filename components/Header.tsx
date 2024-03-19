1d431505022f1200ca447c17030cff216f541981
kelly-keating <keating.kel@gmail.com>
2024-03-19 17:13:32 +1300

    small menu is burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { Cross as Burger } from 'hamburger-react'
 
 import Name from './Name'
+import { useState } from 'react'
 
 function Header() {
   const { pathname } = useRouter()
+  const [isOpen, setOpen] = useState(false)
   
   return (
     <header className="navbar">
       {pathname !== '/' && <Link className="navbar_logo__link" href="/"><Name className="navbar_logo" /></Link>}
-
-      {/* TODO: Burger on mobile */}
       <nav>
-        <ul>
+        <button className='hamburger-react__button' onClick={() => setOpen(!isOpen)}>
+          <p>Menu</p>
+          <Burger toggled={isOpen}/>
+        </button>
+        <ul className={isOpen ? 'open' : 'closed'}>
           {pathname !== '/' && <li><Link className="navbar_link" href='/'>Home</Link></li>}
           <li><Link className={(pathname === '/docs' || pathname === '/docs/[fileName]') ? "navbar_link current" : "navbar_link"} href='/docs'>Docs</Link></li>
           <li><Link className={(pathname === '/projects' || pathname === '/projects/[id]') ? "navbar_link current" : "navbar_link"} href='/projects'>Projects</Link></li>
